68fd5ae5a32017057d6ddc84df43d39905d9427d
Brennan McCulloch <bmccullo@ucsc.edu>
2018-05-13 15:30:13 -0700

    Testing development branch files

--- PlayablePrototype/MyGame/js/main.js
+++ PlayablePrototype/MyGame/js/main.js
@@ -1,3 +1,4 @@
+//Development branch version of our main file
 var game = new Phaser.Game(800, 600, Phaser.AUTO); // Creates a 800 x 600 screen in which the game is displayed
 var ledge, platforms, player, door, doorCheck;
 var fwoosh, unlock, echoSound, echoFill;
